73cb2a10fcea4463a060f1e6a27b1317e0508447
Alexandr <Alexandr@Alexandr>
2023-12-03 17:12:55 +0500

    Начал добавлять валидацию

--- src/main/java/ru/ochkasovap/Library/entity/Person.java
+++ src/main/java/ru/ochkasovap/Library/entity/Person.java
@@ -1,11 +1,26 @@
 package ru.ochkasovap.Library.entity;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Component
 public class Person {
+	private static final Calendar today = Calendar.getInstance();
+	private static final long actualYear = today.get(Calendar.YEAR);
 	private int id;
+	
+	@NotNull(message = "Поле ФИО не должно быть пустым")
 	private String name;
+	
+	@Min(value = 1900, message = "Не корректный год рождения")
+	@Max(value = actualYear)
+	@NotNull(message = "Поле \"год\" не должно быть пустым")
 	private int yearOfBirth;
 
 	public Person() {
